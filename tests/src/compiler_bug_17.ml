3c79ce67c6f46b8633310ffe8603c173a2c6ef2f
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-08-19 22:55:29 +0200

    Adapt to rocq-prover/rocq#22372.

--- tests/src/compiler_bug_17.ml
+++ tests/src/compiler_bug_17.ml
@@ -5,15 +5,19 @@ open Tac2externals
 
 let define s = define { mltac_plugin = "compiler-bugs.compiler_bug_17"; mltac_tactic = s }
 
-let state : int ref = Summary.ref ~name:"tac2compile_bug17" 0
+let state : int Summary.Ref.t = Summary.ref ~name:"tac2compile_bug17" 0
 let push  : unit -> int  = fun () ->
+  let open Summary.Ref in
   state := 1 + !state; !state
 let pop   : int -> unit  = fun i ->
+  let open Summary.Ref in
   if i == !state then
     state := i - 1
   else
     raise Not_found
-let reset : unit -> unit = fun _ -> state := 0
+let reset : unit -> unit =
+  let open Summary.Ref in
+  fun _ -> state := 0
 
 let _ = define "push"  (unit @-> ret int) push
 let _ = define "pop"   (int @-> ret unit) pop
